09f1ed063e23b0845beafb4df558ed73c221b348
Kisara <kisarawenu@gmail.com>
2020-12-22 17:47:22 +0530

    Final Version

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let body = document.getElementById('body');
 let btn = document.getElementById('nightMode');
 let footer = document.getElementById('footer');
 let head = document.getElementById('head');
-let payBtn = document.querySelector('#pay');
+let pay = document.getElementById('pay');
 let priceText01 = document.querySelectorAll('.title')[0];
 let priceText02 = document.querySelectorAll('.title')[1];
 let priceText03 = document.querySelectorAll('.title')[2];
@@ -12,12 +12,32 @@ btn.addEventListener('click' , changeBg);
 
 
 function changeBg(){
-	nightText.classList.toggle("darkTheme");
 	body.classList.toggle("darkTheme");
 	footer.classList.toggle("lightTheme");
-	console.log(payBtn);
-	payBtn.classList.toggle('lightTheme');
 	priceText01.classList.toggle('lightTheme01');
 	priceText02.classList.toggle('lightTheme01');
 	priceText03.classList.toggle('lightTheme01');
+	
+	if(btn.classList.contains('hello')){
+		btn.classList.remove('hello');
+		btn.style.backgroundColor = "#000";
+		btn.style.color = "#fff";
+		btn.innerHTML = "Night Mode";
+	}else{
+		btn.style.backgroundColor = "#c16b08";
+		btn.style.color = "#fff";
+		btn.className = "hello";
+		btn.innerHTML = "Day Mode";
+	}
+
+	if(pay.classList.contains('hello')){
+		pay.classList.remove('hello');
+		pay.style.backgroundColor = "#000";
+		pay.style.color = "#fff";
+	}else{
+		pay.style.backgroundColor = "#fff";
+		pay.style.color = "#000";
+		pay.className = "hello";
+	}
 }
+
